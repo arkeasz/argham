57537f2c9c7ff5560df3135232b144f863f4cb81
Arki <107225218+arkeasz@users.noreply.github.com>
2025-12-08 18:47:07 -0500

    Refactor calculator into argham module
    Removed calculator program and replaced with argham module.

--- main.f90
+++ main.f90
@@ -1,34 +1,11 @@
-program calculator
+module argham
     use class_parser
     use ast
     use evaluator
     use symtab
-    use, intrinsic :: iso_fortran_env, only: dw => real64
-    implicit none
-    real(kind=dw) :: result
-    character(len=32) :: eq
-    character(len=32) :: variables
-    type(Parser) :: p
-    type(ASTNode), pointer :: root
-    type(Vars), pointer :: v 
-
-    variables = "x=2;y=3"
-    eq = "(0.5-x)^(1/3)"
-    p = new_parser(eq)
-    v => new_symtab()
-    call tokenizer(p)
-    call parse(root, p)
-    ! call symtab_add(v, 'y', 1.0_dw)
-    ! call symtab_add(v, 'x', 4.5_dw)
-    call symtab_from_string(v, variables)
-    call symtab_print(v)
-    if (associated(root)) then
-        result = eval(root, v)
-        print *, "Equation: ", eq
-        print *, 'Result =', result
-        ! call print_tree(root)
-    else
-        print *, 'No AST produced (root is null).'
-    end if
-
-end program calculator
+    use math
+    use ansi_colors
+    use evaluator
+    use tokens
+    use utils
+end module argham
